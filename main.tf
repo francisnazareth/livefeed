13e03031294fa5544018556020b6323563504bb5
francisnazareth <73844173+francisnazareth@users.noreply.github.com>
2022-11-14 09:52:48 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,15 +95,15 @@ resource "azurerm_subnet" "pe-subnet" {
   private_link_service_network_policies_enabled  = true
 }
 
-resource "azurerm_route_table" "rt-hub-firewall" {
-  name                          = "route-to-hub-firewall"
+resource "azurerm_route_table" "rt-default-np-firewall" {
+  name                          = "route-aks-default-np-to-nva"
   location                      = var.location
   resource_group_name           = azurerm_resource_group.spoke-rg.name
   disable_bgp_route_propagation = false
 
   route {
-    name                        = "route_internal_traffic_in_vnet"
-    address_prefix              = var.spoke-vnet-address-space
+    name                        = "route_default_np_traffic_in_subnet"
+    address_prefix              = var.aks-default-subnet-address-space
     next_hop_type               = "VnetLocal"
   }
 
@@ -121,68 +121,68 @@ resource "azurerm_route_table" "rt-hub-firewall" {
   }
 }
 
-resource "azurerm_subnet_route_table_association" "aks-default-subnet-to-route-table" {
-  subnet_id      = azurerm_subnet.aks-default-nodepool-subnet.id
-  route_table_id = azurerm_route_table.rt-hub-firewall.id
-}
+resource "azurerm_route_table" "rt-acquisition-np-firewall" {
+  name                          = "route-aks-acquisition-np-to-nva"
+  location                      = var.location
+  resource_group_name           = azurerm_resource_group.spoke-rg.name
+  disable_bgp_route_propagation = false
 
-resource "azurerm_subnet_route_table_association" "aks-imageprocessing-subnet-to-route-table" {
-  subnet_id      = azurerm_subnet.aks-imageprocessing-nodepool-subnet.id
-  route_table_id = azurerm_route_table.rt-hub-firewall.id
-}
-
-resource "azurerm_subnet_route_table_association" "aks-acquisition-subnet-to-route-table" {
-  subnet_id      = azurerm_subnet.aks-acquisition-nodepool-subnet.id
-  route_table_id = azurerm_route_table.rt-hub-firewall.id
-}
-
-resource "azurerm_kubernetes_cluster" "private-aks" {
-  name                       = "aks-${var.customer-name}-${var.env-prefix}-${var.location-prefix}-01"
-  location                   = var.location
-  resource_group_name        = azurerm_resource_group.spoke-rg.name
-  dns_prefix                 = "aks-${var.customer-name}-web"
-  private_cluster_enabled    = true
-  
-  sku_tier            = "Paid"
-  node_resource_group = "rg-aksnode-${var.customer-name}-${var.env-prefix}-${var.location-prefix}-01"
-  azure_policy_enabled = true
-
-  default_node_pool {
-    name       = "systempool"
-    enable_auto_scaling = true
-    min_count  = 3
-    max_count  = 5
-    node_count = 3
-    max_pods   = 30
-    os_disk_size_gb = 128
-    os_disk_type = "Managed"
-    zones = [1, 2, 3]
-    vm_size        = var.aks-system-nodepool-vm-size
-    vnet_subnet_id = azurerm_subnet.aks-default-nodepool-subnet.id
-    type           = "VirtualMachineScaleSets" 
+  route {
+    name                        = "route_default_np_traffic_in_subnet"
+    address_prefix              = var.aks-acquisition-subnet-address-space
+    next_hop_type               = "VnetLocal"
   }
 
-  identity {
-    type = "SystemAssigned"
+  route {
+    name                        = "route_all_traffic_to_fw"
+    address_prefix              = "0.0.0.0/0"
+    next_hop_type               = "VirtualAppliance"
+    next_hop_in_ip_address      = var.firewall-private-ip
   }
 
-  network_profile {
-    network_plugin       = "azure"
-    network_policy       = "calico"
-    load_balancer_sku    = "standard"
-    outbound_type        = "userDefinedRouting"  
-    dns_service_ip       = "172.17.192.10"
-    docker_bridge_cidr   = "172.17.196.0/24"
-    service_cidr         = "172.17.192.0/22" 
-  }
-
-  oms_agent {
-    log_analytics_workspace_id = var.la-workspace-resource-id
-  }  
-  
   tags = {
     Environment  = var.environment,
     CreatedBy    = var.createdby,
     CreationDate = var.creationdate
   }
+}
+
+resource "azurerm_route_table" "rt-imageprocessing-np-firewall" {
+  name                          = "route-aks-imageprocessing-np-to-nva"
+  location                      = var.location
+  resource_group_name           = azurerm_resource_group.spoke-rg.name
+  disable_bgp_route_propagation = false
+
+  route {
+    name                        = "route_default_np_traffic_in_subnet"
+    address_prefix              = var.aks-imageprocessing-subnet-address-space
+    next_hop_type               = "VnetLocal"
+  }
+
+  route {
+    name                        = "route_all_traffic_to_fw"
+    address_prefix              = "0.0.0.0/0"
+    next_hop_type               = "VirtualAppliance"
+    next_hop_in_ip_address      = var.firewall-private-ip
+  }
+
+  tags = {
+    Environment  = var.environment,
+    CreatedBy    = var.createdby,
+    CreationDate = var.creationdate
+  }
+}
+resource "azurerm_subnet_route_table_association" "aks-default-subnet-to-route-table" {
+  subnet_id      = azurerm_subnet.aks-default-nodepool-subnet.id
+  route_table_id = azurerm_route_table.rt-default-np-firewall.id
+}
+
+resource "azurerm_subnet_route_table_association" "aks-imageprocessing-subnet-to-route-table" {
+  subnet_id      = azurerm_subnet.aks-imageprocessing-nodepool-subnet.id
+  route_table_id = azurerm_route_table.rt-imageprocessing-np-firewall.id
+}
+
+resource "azurerm_subnet_route_table_association" "aks-acquisition-subnet-to-route-table" {
+  subnet_id      = azurerm_subnet.aks-acquisition-nodepool-subnet.id
+  route_table_id = azurerm_route_table.rt-acquisition-np-firewall.id
 }
